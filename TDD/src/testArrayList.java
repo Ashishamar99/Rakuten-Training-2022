62a6cc473e932c7868873384358158c39917d002
Ashish Amar <ashishamar1999@gmail.com>
2022-03-29 16:02:36 +0530

    Wrote a test case with expected property and a list.clear()

--- TDD/src/testArrayList.java
+++ TDD/src/testArrayList.java
@@ -4,12 +4,20 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class testArrayList {
+	
+	List<String> list;
+	
+	@Before
+	public void init() {
+		list = new ArrayList<>();
+	}
+	
 	@Test
 	public void testAdd() {
-		List<String> list = new ArrayList<>();
 		boolean result = list.add("Ram");
 		assertTrue(result);
 		//We can also use assertEquals(1, list.size()) instead os assertTrue.
@@ -18,12 +26,27 @@ public class testArrayList {
 	
 	@Test
 	public void testRemove() {
-		List<String> list = new ArrayList<>();
 		list.add("Ram");
 		list.add("Shaym");
 		list.add("Bhaym");
 		
 		String result = list.remove(2);
 		assertEquals("Bhaym", result);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testRemoveFromBlank() {
+		String result = list.remove(0);
+		assertEquals("Ram", result);
+	}
+	
+	@Test
+	public void testClear() {
+		list.add("Ram");
+		list.add("Shaym");
+		list.add("Bhaym");
+		
+		list.clear();
+		assertEquals(0, list.size());
 	}
 }
