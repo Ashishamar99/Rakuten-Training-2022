bb157152604baf06832f7048f1694b4504002d6d
Ashish Amar <ashishamar1999@gmail.com>
2022-03-29 11:52:09 +0530

    Written a test case for subtract.

--- TDD/src/Calculator.java
+++ TDD/src/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
 			return b;
 		return a+b;
 	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
 }
--- TDD/src/CalculatorTest.java
+++ TDD/src/CalculatorTest.java
@@ -1,24 +1,35 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+	
+	Calculator calc;
+	@Before //Add this annotation to run this method before every test case.
+	public void init() {
+		calc = new Calculator(); //Arrange
+	}
 
 	// Convention for writing test cases is write a failing test case, correct it and then write a passing testcase.
 	@Test
 	public void testAdd() {
 		// The 3 A's of testing. 
-		// A-> Arrange, A-> Act, A-> Assert
-		Calculator calc = new Calculator(); //Arrange
+		// A-> Arrange, A-> Act, A-> Asserts
 		int res = calc.add(1, 2); //Act
 		assertEquals(3, res); //Assert
 	}
 	
 	@Test
 	public void testAddWithZero() {
-		Calculator calc = new Calculator();
 		int res = calc.add(0, 2);
 		assertEquals(2, res);
+	}
+	
+	@Test
+	public void testSubtract() {
+		int res = calc.subtract(10, 5);
+		assertEquals(5, res);
 	}
 
 }
