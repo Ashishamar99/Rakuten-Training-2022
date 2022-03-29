9b0319309a551437fb917ccda12a0c127f664223
Ashish Amar <ashishamar1999@gmail.com>
2022-03-29 14:58:59 +0530

    Added a test suite for testing multiple classes at ONCE.

---
+++ TDD/src/AllTestsUsingTestSuite.java
@@ -0,0 +1,19 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import calc.programs.CalculatorTest;
+import calc.testing.testAddComplete;
+
+@RunWith(Suite.class)
+@SuiteClasses({CalculatorTest.class, testAddComplete.class})
+
+
+/* WE WILL ADD ALL THE REQUIRED MULTIPLE TEST CASE FILES
+ * UNDER THE ANNOTATION "SuiteClasses" and run this single test suite
+ * to run all the test cases at ONCE!!!!! */
+
+
+public class AllTestsUsingTestSuite {
+
+}
--- TDD/src/calc/testing/InitialRequrements.java
+++ TDD/src/calc/testing/InitialRequrements.java
@@ -1,9 +1,7 @@
 package calc.testing;
 
-import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.Test;
 import calc.programs.*;
 
 public class InitialRequrements {
