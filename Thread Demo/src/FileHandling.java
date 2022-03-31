a5e78acbc056e774cbffce8f21d7a6f742f3c1c8
Ashish Amar <ashishamar1999@gmail.com>
2022-03-31 15:52:25 +0530

    Reading files using scanner and streams as well!!!

--- Thread Demo/src/FileHandling.java
+++ Thread Demo/src/FileHandling.java
@@ -1,15 +1,48 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class FileHandling {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		readFile();
+		readFileUsingBufferedReaderAndFileReader();
+		readFileUsingScanner();
+		readFileUsingStreams();
 	}
 
-	private static void readFile() throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
+	private static void readFileUsingStreams() throws FileNotFoundException, IOException {
+		FileReader reader = new FileReader("input.txt");
+		BufferedReader bufferedReader = new BufferedReader(reader);
+		
+		Stream<String> myStream = bufferedReader.lines();
+		myStream.forEach((line) -> System.out.println(line));
+		
+	}
+
+	private static void readFileUsingScanner() {
+		try {
+			
+			FileInputStream fs = new FileInputStream("input.txt");
+			Scanner sc = new Scanner(fs);
+			
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());;
+			}
+			
+			sc.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	private static void readFileUsingBufferedReaderAndFileReader() throws FileNotFoundException, IOException {
+		
 		FileReader reader = new FileReader("input.txt");
 		BufferedReader bufferedReader = new BufferedReader(reader);
 		
