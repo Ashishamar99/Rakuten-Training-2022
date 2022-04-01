3949366c911f72b829d097f97a4328202645e37f
Ashish Amar <ashishamar1999@gmail.com>
2022-04-01 15:14:14 +0530

    Wrote some more JS code to process HTML elements.

--- web2/WebContent/app.js
+++ web2/WebContent/app.js
@@ -1,4 +1,17 @@
 function save(){
 	const fnameElement = document.getElementById("firstname");
-	console.info(fnameElement.value);
+	console.log(fnameElement.value);
+	
+	const ageElement = document.getElementById("ageinp");
+	console.log(ageElement.value);
+	
+	const inputElements = document.getElementsByTagName("input");
+	console.info(inputElements);
+	
+	for(let i=0; i< inputElements.length; i++){
+		let element = inputElements[i];
+		if(element.type === "radio" && element.checked===true){
+			console.info(`Checked Radio = ${element.value}`);
+		}
+	}
 }
