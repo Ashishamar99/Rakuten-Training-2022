e82bc76056c850b79f4cfa3e5befe25040d77d42
Ashish Amar <ashishamar1999@gmail.com>
2022-04-06 16:04:25 +0530

    Created an Order Management System.
    With an order controller, and an order service.

---
+++ oms/src/main/java/com/rakuten/OrderService.java
@@ -0,0 +1,22 @@
+package com.rakuten;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+	
+	List<Order> orders = new ArrayList<>();
+	
+	void save(Order order) {
+		System.out.println(order.getItem());
+		System.out.println(order.getPrice());
+		orders.add(order);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+}
