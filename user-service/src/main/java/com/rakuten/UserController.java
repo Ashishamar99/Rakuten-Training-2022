3c6ae845c3f9db148097c8792c231382c23b08ee
Ashish Amar <ashishamar1999@gmail.com>
2022-04-06 11:32:28 +0530

    Added path variables and methods to process them

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -2,8 +2,10 @@ package com.rakuten;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +22,45 @@ public class UserController {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
 		users.add(user);
-		System.out.println("User Saved.");
 	}
 	
 	@GetMapping
 	List<User> getUsers() {
 		return users;
+	}
+	
+	@GetMapping("{name}")
+	User getUserFromName(@PathVariable String name) {
+		System.out.println(name);
+		
+//		users.forEach((User single_user) -> {
+//			if(single_user.getName().equals(name)) {
+//				return single_user;
+//			}
+//		});
+		
+		/*
+		 * USING STREAMS!!
+		 * 
+		 * List<User> filteredUsers = users.stream().filter((user)->user.getName().equals(name)).collect(Collectors.toList());
+			return filteredUsers;
+		 */
+		
+		
+		for(int i=0; i<users.size(); i++) {
+			if(users.get(i).getName().equals(name)) {
+				return users.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@GetMapping("age/{age}")
+	List<User> getUserFromAge(@PathVariable int age) {
+		System.out.println(age);
+		 List<User> filteredUsers = users.stream().filter((user)->user.getAge() == age).collect(Collectors.toList());
+		 return filteredUsers;
+		
 	}
 }
