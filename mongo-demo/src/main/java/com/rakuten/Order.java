712c4ca94db3a0ef7fa068388f2861b3502731a5
Ashish Amar <ashishamar1999@gmail.com>
2022-04-08 15:51:21 +0530

    Added a repository and used that.
    Specified the collection name, got the URI from mongo atlas (like how we connect to mongo compass, same way we connect our application)

--- mongo-demo/src/main/java/com/rakuten/Order.java
+++ mongo-demo/src/main/java/com/rakuten/Order.java
@@ -1,6 +1,14 @@
 package com.rakuten;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "orders") // This is used for mongo db, same as @Entity in SQL databases.
 public class Order {
+	
+	@MongoId // This is the same as @Id in SQL (javax.persistance) package.
+	private String Id;
+	
 	private String item;
 	private float price;
 
@@ -20,5 +28,13 @@ public class Order {
 
 	public void setPrice(float price) {
 		this.price = price;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
 	}
 }
--- mongo-demo/src/main/java/com/rakuten/OrderService.java
+++ mongo-demo/src/main/java/com/rakuten/OrderService.java
@@ -3,17 +3,21 @@ package com.rakuten;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 	
+	@Autowired
+	OrderRepository orderRepository;
+	
 	List<Order> orders = new ArrayList<>();
 	void save(Order order) {
-		orders.add(order);
+		orderRepository.save(order);
 	}
 	public List<Order> getAllOrders() {
-		return orders;
+		return orderRepository.findAll();
 	}
 	
 	
