122edaf64bf762212a15a99720e22fd828aab1bf
Ashish Amar <ashishamar1999@gmail.com>
2022-04-12 14:04:30 +0530

    Assignment 2 done. Formatted both assignment codes.

---
+++ Module2Assessment/src/FooCorporation.java
@@ -0,0 +1,24 @@
+
+public class FooCorporation {
+	public static void main(String[] args) {
+		calculatePay(7.5, 35);
+		calculatePay(8.20, 47);
+		calculatePay(10.0, 73);
+	}
+
+	private static void calculatePay(double basePay, int hoursWorked) {
+		double totalPay = 0.0;
+		if (basePay < 8.0) {
+			System.out.println("Error: Base pay cannot be less than 8$.");
+		} else if (hoursWorked > 60) {
+			System.out.println("Error: Worker should work for a maximum of 60 hrs");
+		} else if (hoursWorked > 40) {
+			int overtime = hoursWorked - 40;
+			totalPay = (40 * (basePay)) + (overtime * basePay * 1.5);
+			System.out.println("Total Pay = " + totalPay);
+		} else {
+			totalPay = hoursWorked * basePay;
+			System.out.println("Total Pay = " + totalPay);
+		}
+	}
+}
--- Module2Assessment/src/GravityCalculator.java
+++ Module2Assessment/src/GravityCalculator.java
@@ -1,7 +1,7 @@
 
 public class GravityCalculator {
 	/*
-	 * Initial output - 
+	 * Initial output -
 	 * 
 	 * The object's position after 10.0 seconds is 0.0m.
 	 */
@@ -11,14 +11,15 @@ public class GravityCalculator {
 		double fallingTime = 10.0;
 		double initialPosition = 0.0;
 		double finalPosition = 0.0;
-		
+
 		finalPosition = calculateFinalPosition(gravity, fallingTime, initialVelocity, initialPosition);
 		System.out.println("The object's position after " + fallingTime + " seconds is " + finalPosition + "m.");
 	}
 
 	private static double calculateFinalPosition(double gravity, double fallingTime, double initialVelocity,
 			double initialPosition) {
-		double resultPosition = 0.5 * (gravity * Math.pow(fallingTime, 2)) + (initialVelocity * fallingTime) + initialPosition;
+		double resultPosition = 0.5 * (gravity * Math.pow(fallingTime, 2)) + (initialVelocity * fallingTime)
+				+ initialPosition;
 		return resultPosition;
 	}
 }
