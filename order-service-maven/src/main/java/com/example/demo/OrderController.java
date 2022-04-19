3b6d41136241bce46a4ce605055e860cf320f2e7
unknown <rakdev12@iiht.tech>
2022-04-19 15:15:43 +0530

    Wrote some more code for mySQL workbench server and putting records in that db

--- order-service-maven/src/main/java/com/example/demo/OrderController.java
+++ order-service-maven/src/main/java/com/example/demo/OrderController.java
@@ -3,16 +3,16 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/order")
 public class OrderController {
 	
 	@Autowired
 	OrderService orderService;
 	
-	@PostMapping("/order")
 	void createOrder(@RequestBody Order order) {
 		orderService.saveOrder(order);
 	}
--- order-service-maven/src/main/java/com/example/demo/OrderService.java
+++ order-service-maven/src/main/java/com/example/demo/OrderService.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+	
+	@Autowired
+	OrderRepository repository;
+	
 	void saveOrder(Order order) {
-		
+		repository.save(order);
+		System.out.println("Order is saved in the repository.");
 	}
 }
