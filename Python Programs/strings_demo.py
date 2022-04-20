94147e915b87d8ba60c0c16919c15cb0961b90a5
Ashish Amar <ashishamar1999@gmail.com>
2022-04-20 15:47:13 +0530

    Update strings_demo.py

--- Python Programs/strings_demo.py
+++ Python Programs/strings_demo.py
@@ -16,3 +16,25 @@ print((b+"aa aa ").strip())
 print(b.replace("H", "J"))
 print('----')
 
+print('Formatting Strings - ')
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+print('----')
+
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+print('----')
+
+print('Escape Characters -')
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt, "\"ooo for octal value printing.")
+print("This line will be \f printed in 2 pages, 2nd page starts with 'printed...' ")
+print('"\\r\\n" is used for a new line in Windows format whereas, just a "\\n" is used for Linux')
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+print('----')
