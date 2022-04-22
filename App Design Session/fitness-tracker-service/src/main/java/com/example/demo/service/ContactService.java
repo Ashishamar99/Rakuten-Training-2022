0f27b8de79c1221fad3eaa955911d4ca1507ef85
unknown <ashishamar1999@gmail.com>
2022-04-22 14:42:21 +0530

    Created a contact form to process POST messages

--- App Design Session/fitness-tracker-service/src/main/java/com/example/demo/service/ContactService.java
+++ App Design Session/fitness-tracker-service/src/main/java/com/example/demo/service/ContactService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.templates.ContactUs;
+
+@Service
 public class ContactService {
+
+	public void send(ContactUs message) {
+		System.out.println(message);
+		
+	}
 
 }
--- App Design Session/fitness-tracker-service/src/main/java/com/example/demo/templates/ContactUs.java
+++ App Design Session/fitness-tracker-service/src/main/java/com/example/demo/templates/ContactUs.java
@@ -5,4 +5,33 @@ import java.math.BigInteger;
 public class ContactUs {
 	private String name, message, email;
 	private BigInteger phone_number;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public BigInteger getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(BigInteger phone_number) {
+		this.phone_number = phone_number;
+	}
+	@Override
+	public String toString() {
+		return String.format("Name = %s, Mail = %s, Phone = %d, \n Message = %s", this.getName(), this.getEmail(), this.getPhone_number(), this.getMessage());
+	}
+	
 }
