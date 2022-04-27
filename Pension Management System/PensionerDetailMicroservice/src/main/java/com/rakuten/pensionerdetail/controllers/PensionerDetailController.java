a0895fa42537d48b60dfee79523cc8fe57f2b311
Ashish Amar <ashishamar1999@gmail.com>
2022-04-27 20:13:31 +0530

    Pensioner Detail Microservice is done!!!!

--- Pension Management System/PensionerDetailMicroservice/src/main/java/com/rakuten/pensionerdetail/controllers/PensionerDetailController.java
+++ Pension Management System/PensionerDetailMicroservice/src/main/java/com/rakuten/pensionerdetail/controllers/PensionerDetailController.java
@@ -2,17 +2,36 @@ package com.rakuten.pensionerdetail.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rakuten.pensionerdetail.entities.PensionerDetail;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/PensionerDetailByAadhaar")
 public class PensionerDetailController {
 	
-	@GetMapping("/{m}")
-	String demoget(@PathVariable String m) {
-		return m;
+	List<PensionerDetail> pensioners = new ArrayList<>();
+	
+	@GetMapping("/{aadhar_number}")
+	List<PensionerDetail> getPensionerDetail(@PathVariable String aadhar_number) {
+		
+		List<PensionerDetail> search_result = pensioners.stream().filter(saved_pensioner -> String.valueOf(saved_pensioner.getAadhar_number()).equals(aadhar_number)).collect(Collectors.toList());
+		return search_result;
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void savePensionerDetail(@RequestBody PensionerDetail pensioner_detail) {
+		pensioners.add(pensioner_detail);
 	}
 }
