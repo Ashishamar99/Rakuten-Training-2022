945d767bcadf7e5688e3365da017bc4a7c2ea6d8
Ashish Amar <ashishamar1999@gmail.com>
2022-05-08 00:18:24 +0530

    CRUD category Done!

--- Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/controllers/CategoryController.java
+++ Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/controllers/CategoryController.java
@@ -2,15 +2,24 @@ package com.rakuten.workouttracker.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rakuten.workouttracker.entities.Category;
+import com.rakuten.workouttracker.entities.Workout;
 import com.rakuten.workouttracker.services.CategoryService;
 
 @RestController
@@ -34,5 +43,26 @@ public class CategoryController {
 	void saveCategories(@RequestBody List<Category> listOfCategories) {
 		categoryService.addMultipleCategories(listOfCategories);
 	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void updateCategoryById(@PathVariable Integer id, @RequestBody Category category) {
+		categoryService.updateCategoryFromId(id, category);
+	}
+	
+	@GetMapping("/{id}")
+	Optional<Category> getCategoryById(@PathVariable Integer id) {
+		return categoryService.fetchCategoryById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.ACCEPTED)
+	void removeCategoryById(@PathVariable Integer id) {
+		categoryService.deleteCategoryById(id);
+	}
+	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	void handleDeleteInvalidCategory() { }
 
 }
--- Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/services/CategoryService.java
+++ Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/services/CategoryService.java
@@ -2,11 +2,13 @@ package com.rakuten.workouttracker.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rakuten.workouttracker.entities.Category;
+import com.rakuten.workouttracker.entities.Workout;
 import com.rakuten.workouttracker.repositories.CategoryRepository;
 
 @Service
@@ -28,6 +30,21 @@ public class CategoryService {
 
 	public void addMultipleCategories(List<Category> listOfCategories) {
 		listOfCategories.forEach(category -> categoryRepository.save(category));
+		
+	}
+
+	public void updateCategoryFromId(Integer id, Category category) {
+		category.setId(id);
+		categoryRepository.save(category);
+		
+	}
+
+	public Optional<Category> fetchCategoryById(Integer id) {
+		return categoryRepository.findById(id);
+	}
+
+	public void deleteCategoryById(Integer id) {
+		categoryRepository.deleteById(id);
 		
 	}
 }
