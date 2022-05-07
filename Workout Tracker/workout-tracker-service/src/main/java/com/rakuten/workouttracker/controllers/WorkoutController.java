4c777c34b7a06f4ff1133f5e390a42f13b5139b2
Ashish Amar <ashishamar1999@gmail.com>
2022-05-08 00:41:09 +0530

    Searching a workout by category done

--- Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/controllers/WorkoutController.java
+++ Workout Tracker/workout-tracker-service/src/main/java/com/rakuten/workouttracker/controllers/WorkoutController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,6 +55,11 @@ public class WorkoutController {
 	@ResponseStatus(code = HttpStatus.ACCEPTED)
 	void removeWorkoutById(@PathVariable Integer id) {
 		workoutService.deleteWorkoutById(id);
+	}
+	
+	@GetMapping("/search")
+	List<Workout> getAllWorkoutsOfSameCategory(@RequestParam String category){
+		return workoutService.fetchAllWorkoutsOfSameCategory(category);
 	}
 	
 	@ExceptionHandler(EmptyResultDataAccessException.class)
