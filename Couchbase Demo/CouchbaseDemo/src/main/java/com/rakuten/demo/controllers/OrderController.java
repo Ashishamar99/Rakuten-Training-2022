f0139618747b9a56289bf621167d8990c8d8f739
Ashish Amar <ashishamar1999@gmail.com>
2022-05-19 12:50:57 +0530

    MADE MAJOR CHANGES!
    For some reason, Lombok's @Getter and @Setter doesn't work, had to actually generate them using STS, also Lombok's @Data didn't work.
    
    And I had to create a primary index =, something and also had to write the configurations in a config class than the application.properties.

--- Couchbase Demo/CouchbaseDemo/src/main/java/com/rakuten/demo/controllers/OrderController.java
+++ Couchbase Demo/CouchbaseDemo/src/main/java/com/rakuten/demo/controllers/OrderController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,5 +30,11 @@ public class OrderController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void saveOrder(@RequestBody Order order) {
 		orderService.saveOrder(order);
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(code = HttpStatus.ACCEPTED)
+	public void deleteAllOrders() {
+		orderService.deleteAll();
 	}
 }
